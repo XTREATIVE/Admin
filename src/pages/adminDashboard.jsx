1078390eb8461426ec7920177064884c69706e88
Blessedimmaculate <nowenakazibwe7@gmail.com>
2025-09-24 10:56:59 +0300

    Display of the dashboard

--- src/pages/adminDashboard.jsx
+++ src/pages/adminDashboard.jsx
@@ -1,27 +1,27 @@
-// // pages/AdminDashboard.js
-// import React from "react";
-// import Sidebar from "../components/sidebar";
-// import Header from "../components/header";
-// import StatsCardsGrid from "../components/cardgrid";
+// pages/AdminDashboard.js
+import React from "react";
+import Sidebar from "../components/sidebar";
+import Header from "../components/header";
+import StatsCardsGrid from "../components/cardgrid";
 
-// const AdminDashboard = () => {
-//   const dummyNotifications = [
-//     { title: "New vendor registered", time: "5 mins ago" },
-//     { title: "Customer placed an order", time: "10 mins ago" },
-//     { title: "New loan application", time: "30 mins ago" },
-//   ];
+const AdminDashboard = () => {
+  const dummyNotifications = [
+    { title: "New vendor registered", time: "5 mins ago" },
+    { title: "Customer placed an order", time: "10 mins ago" },
+    { title: "New loan application", time: "30 mins ago" },
+  ];
 
-//   return (
-//     <div className="h-screen font-poppins">
-//       <Header notifications={dummyNotifications} />
-//       <div className="flex flex-1 overflow-hidden">
-//         <Sidebar />
-//         <main className="flex-1 p-4 bg-gray-100 ml-[80px]">
-//           <StatsCardsGrid />
-//         </main>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className="h-screen font-poppins">
+      <Header notifications={dummyNotifications} />
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar />
+        <main className="flex-1 p-4 bg-gray-100 ml-[80px]">
+          <StatsCardsGrid />
+        </main>
+      </div>
+    </div>
+  );
+};
 
-// export default AdminDashboard;
+export default AdminDashboard;
